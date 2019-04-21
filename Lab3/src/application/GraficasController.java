cd3bcf7c8c0588211aeb3dd2e94291ea1fef8a30
camiilocq <38769518+camiilocq@users.noreply.github.com>
2019-04-21 17:01:07 -0500

    this has been changed

--- Lab3/src/application/GraficasController.java
+++ Lab3/src/application/GraficasController.java
@@ -1,9 +1,45 @@
 package application;
 
+import java.io.IOException;
+
+import javafx.event.Event;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.DatePicker;
+import javafx.stage.Stage;
+
 public class GraficasController {
 
-	public GraficasCotroller() {
-		// TODO Auto-generated constructor stub
+	private DatePicker inicio;
+	private DatePicker fin;
+	private CheckBox divisas;
+	private CheckBox acciones;
+	private Button cargar;
+	private Button volver;
+	
+	
+	public void initialize() {
+		
 	}
-
+	
+	public void cargarDatos() {
+		
+	}
+	
+	public void volver(Event event) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("Sample.fxml"));
+			Scene scene = new Scene(root);
+			Stage windows = (Stage)((Node)event.getSource()).getScene().getWindow();
+			
+			windows.setScene(scene);
+			windows.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
