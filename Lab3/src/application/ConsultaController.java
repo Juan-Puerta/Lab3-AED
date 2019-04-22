aa9d5cd838ee573778c47fd6f4b8c42f00cf7d1b
camiilocq <38769518+camiilocq@users.noreply.github.com>
2019-04-21 19:01:24 -0500

    this has been changed

--- Lab3/src/application/ConsultaController.java
+++ Lab3/src/application/ConsultaController.java
@@ -1,8 +1,11 @@
 package application;
 
+import java.io.File;
 import java.io.IOException;
 
 import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
@@ -10,26 +13,53 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.RadioButton;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class ConsultaController {
 
-	private DatePicker inicio;
-	private DatePicker fin;
-	private CheckBox divisas;
-	private CheckBox acciones;
-	private Button cargar;
-	private Button pAltos;
-	private Button pBajos;
-	private Button periodosMayores;
-	private Button volver;
+	@FXML private DatePicker inicio;
+	@FXML private DatePicker fin;
+	@FXML private RadioButton divisas;
+	@FXML private RadioButton acciones;
+	@FXML private Button cargar;
+	@FXML private Button pAltos;
+	@FXML private Button pBajos;
+	@FXML private Button periodosMayores;
+	@FXML private Button volver;
 	
 	public void initialize() {
-		
+		divisas.setOnMouseClicked(new EventHandler<Event>() {
+
+			@Override
+			public void handle(Event event) {
+				if(acciones.isSelected()) {
+					acciones.setSelected(false);
+				}
+				
+			}
+		});
+			
+		acciones.setOnMouseClicked(new EventHandler<Event>() {
+
+			@Override
+			public void handle(Event event) {
+				if(divisas.isSelected()) {
+					divisas.setSelected(false);
+				}
+			}
+		});		 
 	}
 	
 	public void cargarDatos() {
-		
+		FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Buscar base de datos");
+
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("All txt", "*.*"));
+
+        File imgFile = fileChooser.showOpenDialog(null);
+
 	}
 	
 	public void precioMasAlto() {
